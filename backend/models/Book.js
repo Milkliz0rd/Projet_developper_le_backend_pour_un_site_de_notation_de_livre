e5fb6557781e0ac6612859cd6ffae74e1476491e
Alexandre Morozoff <morozoff_alexandre@outlook.com>
2025-01-03 11:20:16 +0100

    Book schema complited

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -1,11 +1,41 @@
 const mongoose = require("mongoose");
 
 const bookSchema = mongoose.Schema({
-  title: { type: String, required: true, description: "titre du livre" },
-  description: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  userId: { type: String, required: true },
-  price: { type: Number, required: true },
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+    description: "Identifiant MongoDB unique de l'utilisateur",
+  },
+  title: { type: String, required: true, description: "Titre du livre" },
+  author: { type: String, required: true, description: "Auteur du livre" },
+  imageUrl: {
+    type: String,
+    required: true,
+    description: "Illustration/couverture du livre",
+  },
+  year: {
+    type: Number,
+    required: true,
+    description: "Année de la publication du livre",
+  },
+  genre: { type: String, required: true, description: "Genre du livre" },
+  ratings: [
+    {
+      userId: {
+        type: String,
+        required: true,
+        description:
+          "Identifiant MongoDB unique de l'utilisateur qui a noté le livre",
+      },
+      grade: {
+        type: Number,
+        requirerd: true,
+        description: "Note donnée à un livre",
+      },
+    },
+  ],
+  averageRating: { type: Number, description: "note moyenne du livre" },
 });
 
 module.exports = mongoose.model("Books", bookSchema);
